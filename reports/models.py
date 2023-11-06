85504bcbc204bbf1801e8772b697dc84b9c54e40
Daniel Coker <danielcoker28@outlook.com>
2023-11-06 18:21:31 +0100

    Add models

--- reports/models.py
+++ reports/models.py
@@ -1,3 +1,37 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+from core.models import SoftDeleteModel, TimestampedModel
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ("id",)
+
+    def __str__(self):
+        return self.name
+
+
+class Report(TimestampedModel, SoftDeleteModel):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+    )
+    description = models.TextField()
+    location = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True,
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name="reports",
+    )
+
+    def __str__(self):
+        return self.id.hex
