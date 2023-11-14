a8bf89c5608dc4758f6d38d264d48a2c54b003f3
Daniel Coker <danielcoker28@outlook.com>
2023-11-14 04:23:10 +0100

    Remove unused import

--- authentication/views.py
+++ authentication/views.py
@@ -2,7 +2,7 @@ from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
-from rest_framework.viewsets import ModelViewSet, ViewSet
+from rest_framework.viewsets import ViewSet
 
 from authentication.serializers import (
     SignUpSerializer,
