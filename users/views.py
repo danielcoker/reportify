f224e5dbd227dabf309a95e017aeceba04e46188
Daniel Coker <danielcoker28@outlook.com>
2023-11-14 03:50:40 +0100

    Implement get user endpoint

--- users/views.py
+++ users/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from core.mixins import ResponseMessageMixin
+from users.models import User
+from users.serializers import UserSerializer
+from rest_framework.permissions import IsAuthenticated
+
+class UserViewSet(ResponseMessageMixin, GenericViewSet):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = (IsAuthenticated,)
+
+    @action(detail=False)
+    def me(self, request):
+        serializer = UserSerializer(request.user, context={"request": request})
+
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
