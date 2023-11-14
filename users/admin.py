701303a7ee77eccb30ab55667f4c1e5a8aafa122
Daniel Coker <danielcoker28@outlook.com>
2023-11-14 04:14:30 +0100

    Add user model to admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import User
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        "email",
+        "first_name",
+        "last_name",
+        "phone",
+    )
+
+    search_fields = (
+        "email",
+        "first_name",
+        "last_name",
+        "phone",
+    )
+
+
+admin.site.register(User, UserAdmin)
