3a232350d8c0a8d2add4a6f36725fabb08eb1067
Daniel Coker <danielcoker28@outlook.com>
2023-12-29 04:47:24 +0100

    Load classifier model on app ready

--- reports/apps.py
+++ reports/apps.py
@@ -1,6 +1,11 @@
 from django.apps import AppConfig
 
+from reportify.utils import ReportClassifierModelLoader
+
 
 class ReportsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "reports"
+
+    def ready(self):
+        ReportClassifierModelLoader.load_model()
